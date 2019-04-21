3f7a86ae2105cb4711dd4c524e231214464ba310
leaflu0315 <johnson7511333@gmail.com>
2019-04-21 17:05:06 +0800

    web3js tutorial

--- Tutorial/2_BlockChain/subscribe_block_header.js
+++ Tutorial/2_BlockChain/subscribe_block_header.js
@@ -1,1 +1,10 @@
-const Web3 = require('web3')
+const Web3 = require('web3');
+
+let ws = 'wss://mainnet.infura.io/ws';
+
+var web3 = new Web3(ws);
+
+web3.eth.subscribe('newBlockHeaders',(err,blockheader)=>{
+    if(err) console.log(err);
+    else console.log(blockheader);
+});
